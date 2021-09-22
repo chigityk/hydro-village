9439ca4da5e22dbc889c7601ea78ca0dd1bcba24
Chris Chiquet <chiquet.chris@gmail.com>
2021-09-22 07:36:24 -0700

    Defined main app container weight *accidentally removed new windows

--- hydroponic_system/hydrogui.py
+++ hydroponic_system/hydrogui.py
@@ -1,41 +1,16 @@
 import tkinter as tk
-from tkinter import Button, Frame, Grid, Image, Label, PhotoImage, Tk, ttk, font
+from tkinter import Button, Frame, Grid, Image, Label, PhotoImage, ttk, font
 import time
 import os
 timeString = time.strftime('%I:%M %p')
 
 os.chdir('hydroponic_system')
+
 main = tk.Tk()
 main.title('Lettuce Wall')
 main.configure(background='#02394A', width=700, height=700)
-
-# Nutrients window setup
-
-def nutewindow():
-    nutrients = Tk()
-    nutrients.title('Nutrient Configuration')
-    nutrients.configure(width=500,
-                        height=500,
-                        background='#02394A')
-
-
-# Lights window setup
-
-def lightswindow():
-    lights = Tk()
-    lights.title('Lights Configure')
-    lights.configure(width=500,
-                        height=500,
-                        background='#02394A')
-
-# Settings window setup
-
-def settingswindow():
-    settings = Tk()
-    settings.title('Settings')
-    settings.configure(width=500,
-                        height=500,
-                        background='#02394A')
+main.columnconfigure(0, weight=1)
+main.rowconfigure(0, weight=1)
 
 img_fanoff = PhotoImage(file='assets/fanoff.png')
 img_fanon = PhotoImage(file='assets/fanon.png')
@@ -70,15 +45,15 @@ main.style.configure('settings.TButton',
 
 
 homelbl = ttk.Label(main, text='Hydro Home', background='#02394A')
-homelbl.grid(row=0, column=0)
+homelbl.grid(row=0, column=0, columnspan=4)
 homelbl.configure(background='#02394A', font='notosansbold', foreground='white')
 
 timelbl = ttk.Label(main, text=timeString)
-timelbl.grid(row=0, column=1)
+timelbl.grid(row=0, column=4, columnspan=4)
 timelbl.configure(background='#02394A', font='notosansbold', foreground='white')
 
 templbl = ttk.Label(main, text='Local Temp')
-templbl.grid(row=0, column=2)
+templbl.grid(row=0, column=8, columnspan=4, sticky='e')
 templbl.configure(background='#02394A', font='notosansbold', foreground='white')
 
 # Nutrients Button, clicks into sub-menu
@@ -87,19 +62,21 @@ nutebtn = tk.Button(main,
                         bg='#02394A',
                         activebackground='lightblue',
                         fg='white',
-                        font=('notosansbold', 15),
-                        command=nutewindow)
-nutebtn.grid(row=1, column=0)
+                        height=3,
+                        width=12,
+                        font=('notosansbold', 15))
+nutebtn.grid(row=1, column=2, rowspan=2, columnspan=4, padx=5, pady=5)
 
 # Lights Button, clicks into sub-menu
 lightsbtn = tk.Button(main,
-                        text='Lights',
+                        text=' Lights  ',
                         bg='#02394A',
                         activebackground='lightblue',
                         fg='white',
-                        font=('notosansbold', 15),
-                        command=lightswindow)
-lightsbtn.grid(row=1, column=2)
+                        height=3,
+                        width=12,
+                        font=('notosansbold', 15))
+lightsbtn.grid(row=1, column=6, rowspan=2, columnspan=4, padx=5, pady=5)
 
 #fan on/off button
 # Define our switch function
@@ -121,10 +98,9 @@ fanbtn = tk.Button(main,
                 command=fanSwitch,
                 border=0,
                 highlightthickness=0,
-                borderwidth=0,
                 height=100,
                 width=100)
-fanbtn.grid(row=2, column=0)
+fanbtn.grid(row=3, column=1, rowspan=3, columnspan=3)
 
 #o2 pump on/off switch
 
@@ -150,7 +126,7 @@ o2btn = tk.Button(main,
                 highlightthickness=0,
                 height=100,
                 width=100)
-o2btn.grid(row=2, column=1)
+o2btn.grid(row=3, column=4, rowspan=3, columnspan=3)
 
 #water pump on/off switch
 
@@ -176,7 +152,7 @@ h2obtn = tk.Button(main,
                 highlightthickness=0,
                 height=100,
                 width=100)
-h2obtn.grid(row=2, column=2)
+h2obtn.grid(row=3, column=7, rowspan=3, columnspan=3)
 
 
 #settings button
@@ -187,11 +163,9 @@ settingsButton = tk.Button(main,
                            height=70,
                            width=70,
                            border=0,
-                           highlightthickness=0,
-                           activebackground='#02394A',
-                           command=settingswindow
+                           activebackground='#02394A'
                            )
-settingsButton.grid(row=3, column=2)
+settingsButton.grid(row=9, column=9)
 
 
 main.mainloop()
