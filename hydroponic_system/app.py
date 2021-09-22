4a6aba734178934fd882cd7a14b56f8e0ef33429
Chris Chiquet <chiquet.chris@gmail.com>
2021-09-22 09:52:27 -0700

    started simple button command on app

--- hydroponic_system/app.py
+++ hydroponic_system/app.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from gpiozero import LED, Button
+from time import sleep
+
+led = LED(17)
+button = Button(2)
+
+while True:
+    button.wait_for_press()
+    led.toggle()
+    sleep(0.5)
